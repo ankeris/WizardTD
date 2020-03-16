3a43eaba5276d39ad458c9530f57da2d43f97c7d
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-17 00:36:54 +0200

    feat(mobs): spawn specific amount of mobs

--- scripts/level.gd
+++ scripts/level.gd
@@ -5,25 +5,21 @@ extends Node
 var mob = preload("res://scenes/mobs/mob.tscn")
 onready var start_pos = $'GridMap/Navigation/start_pos'
 var radius = 5
-const mobs_count = 5
+const mobs_count = 3
 
-var timer
-
-func _init():
-	timer = Timer.new()
-	add_child(timer)
-	timer.autostart = true
-	timer.wait_time = 1
-	timer.connect("timeout", self, "_timeout")
-	
-func _timeout():
-	var new_mob = mob.instance()
-	new_mob.global_transform.origin = start_pos.global_transform.origin
-	add_child(new_mob)
-	print("Timed out!")
+onready var timer = $'Timer'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for n in range(mobs_count):
+		timer.set_wait_time(1)
+		timer.set_one_shot(true)
+		timer.start()
+		yield(timer, "timeout")
+		var new_mob = mob.instance()
+		new_mob.global_transform.origin = start_pos.global_transform.origin
+		add_child(new_mob)
+
 #	for n in range(mobs_count):
 		
 	
