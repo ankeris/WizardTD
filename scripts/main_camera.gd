1af7d83df18ee391ec428dc50de9d73dacca4595
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-03-21 01:59:07 +0200

    feat(): select a tower node

--- scripts/main_camera.gd
+++ scripts/main_camera.gd
@@ -3,6 +3,7 @@ extends Camera
 const ray_length = 1000
 const tower_spot_collider_name = 'tower_spot_collider'
 onready var towers_path = $'../GridMap/Navigation/towers_path'
+var previously_selected_tower_path_flag;
 
 func _ready():
 	pass
@@ -14,9 +15,17 @@ func _input(event):
 	var raycast_object = space_state.intersect_ray(from, to, [])
 	
 	# handle click
-	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		if (raycast_object.collider.name == tower_spot_collider_name):
-			print("tower_spot")
+	if event is InputEventMouseButton and event.pressed and event.button_index == 1 and raycast_object:
+		var target_collider = raycast_object.collider
+		if (target_collider.name == tower_spot_collider_name):
+			var tower_spot_scene = target_collider.get_parent().get_parent()
+			var tower_spot_flag = tower_spot_scene.get_node("flag")
+			if (previously_selected_tower_path_flag):
+				previously_selected_tower_path_flag.visible = false
+			tower_spot_flag.visible = true
+			tower_spot_flag.rotate_y(rand_range(1,11))
+			previously_selected_tower_path_flag = tower_spot_flag;
+			
 
 # maybe submit a PR for this feature
 
